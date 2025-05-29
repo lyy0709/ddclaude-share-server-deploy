ba8a96a63c036ee0ac1334118e3a070946f4760b
lyy0709 <65712338+lyy0709@users.noreply.github.com>
2025-05-29 18:14:17 +0800

    Update quick-install.sh

--- quick-install.sh
+++ quick-install.sh
@@ -12,7 +12,7 @@ docker compose pull
 docker compose up -d --remove-orphans
 
 ## 提示信息
-echo "服务启动成功，请访问 http://localhost:8300"
+echo "服务启动成功，请访问 http://localhost:8400"
 echo "管理员后台地址 http://localhost:8300/lyy0709"
 echo "管理员账号: admin"
 echo "管理员密码: 123456"
